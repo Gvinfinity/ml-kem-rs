501f48883c6cf5e1d2b6316a7154d581d95382f0
Gvinfinity <gabrielsoares@berry.dev.br>
2026-01-31 00:46:45 -0300

    refactor: Fix dumb pub mod

--- src/cryptographic.rs
+++ src/cryptographic.rs
@@ -1,58 +1,56 @@
-pub mod cryptographic {
-    use std::{array, io::Read};
+use std::{array, io::Read};
 
-    use sha3::{Digest, Sha3_256, Sha3_512, Shake256, digest::{ExtendableOutput, Update}};
+use sha3::{Digest, Sha3_256, Sha3_512, Shake256, digest::{ExtendableOutput, Update}};
 
-    enum ReturnVec {
-        Vec128([u8; 128]),
-        Vec192([u8; 192])
+enum ReturnVec {
+    Vec128([u8; 128]),
+    Vec192([u8; 192])
+}
+
+pub fn prf(eta: usize, s: [u8; 32], b: u8) -> ReturnVec { 
+    assert!(eta == 2 || eta == 3, "n should be 2 or 3");
+
+    let mut hasher = Shake256::default();
+    hasher.update(&s);
+    hasher.update(array::from_ref(&b));
+    
+    let mut reader = hasher.finalize_xof();
+
+    if eta == 2 {
+        let mut output = [0u8; 128];
+        reader.read(&mut output).expect("Failed");
+        return ReturnVec::Vec128(output);
+    } else {
+        let mut output = [0u8; 192];
+        reader.read(&mut output).expect("Failed");
+        return ReturnVec::Vec192(output);
     }
+}
 
-    pub fn prf(eta: usize, s: [u8; 32], b: u8) -> ReturnVec { 
-        assert!(eta == 2 || eta == 3, "n should be 2 or 3");
+pub fn h(s: &[u8]) -> [u8;32] {
+    let mut hasher = Sha3_256::default();
 
-        let mut hasher = Shake256::default();
-        hasher.update(&s);
-        hasher.update(array::from_ref(&b));
-        
-        let mut reader = hasher.finalize_xof();
+    Update::update(&mut hasher, &s);
+    
+    return hasher.finalize().into();
+}
 
-        if eta == 2 {
-            let mut output = [0u8; 128];
-            reader.read(&mut output).expect("Failed");
-            return ReturnVec::Vec128(output);
-        } else {
-            let mut output = [0u8; 192];
-            reader.read(&mut output).expect("Failed");
-            return ReturnVec::Vec192(output);
-        }
-    }
+pub fn j(s: &[u8]) -> [u8;32] {
+    let mut hasher = Shake256::default();
+    let mut output = [0u8; 32];
 
-    pub fn h(s: &[u8]) -> [u8;32] {
-        let mut hasher = Sha3_256::default();
+    hasher.update(&s);
+    hasher.finalize_xof_into(&mut output);
 
-        Update::update(&mut hasher, &s);
-        
-        return hasher.finalize().into();
-    }
+    return output;
+}
 
-    pub fn j(s: &[u8]) -> [u8;32] {
-        let mut hasher = Shake256::default();
-        let mut output = [0u8; 32];
+pub fn g(c: &[u8]) -> ([u8;32], [u8;32]) {
+    let mut hasher = Sha3_512::default();
+    Update::update(&mut hasher, &c);
 
-        hasher.update(&s);
-        hasher.finalize_xof_into(&mut output);
-
-        return output;
-    }
-
-    pub fn g(c: &[u8]) -> ([u8;32], [u8;32]) {
-        let mut hasher = Sha3_512::default();
-        Update::update(&mut hasher, &c);
-
-        let hash: [u8; 64] = hasher.finalize().into();
-        let (left, right) = hash.split_at(32);
-        let tuple = (left.try_into().unwrap(), right.try_into().unwrap());
-        return tuple
-    }
+    let hash: [u8; 64] = hasher.finalize().into();
+    let (left, right) = hash.split_at(32);
+    let tuple = (left.try_into().unwrap(), right.try_into().unwrap());
+    return tuple
 }
--- src/kpke.rs
+++ src/kpke.rs
@@ -1,21 +1,19 @@
-pub mod kpke {
-    use crate::{ParameterSet, SELECTED_PARAMETER_SET, cryptographic::cryptographic::{g, prf}};
+use crate::{SELECTED_PARAMETER_SET, cryptographic::{g, prf}};
 
-    fn KeyGen(d: [u8;32]){
-        let (rho, sigma) = g(&d);
-        let n = 0;
-        let mut a: Vec<Vec<[u8;SELECTED_PARAMETER_SET.n as usize]>> = vec![vec![]; SELECTED_PARAMETER_SET.k as usize];
-        let mut s: Vec<[u8;SELECTED_PARAMETER_SET.n as usize]>;
+fn KeyGen(d: [u8;32]){
+    let (rho, sigma) = g(&d);
+    let n = 0;
+    let mut a: Vec<Vec<[u8;SELECTED_PARAMETER_SET.n as usize]>> = vec![vec![]; SELECTED_PARAMETER_SET.k as usize];
+    let mut s: Vec<[u8;SELECTED_PARAMETER_SET.n as usize]>;
 
-        for i in 0..SELECTED_PARAMETER_SET.k as usize{
-            for j in 0..SELECTED_PARAMETER_SET.k as usize {
-                a[i][j] = sampleNTT(rho, j as u8, i as u8);
-            }
+    for i in 0..SELECTED_PARAMETER_SET.k as usize{
+        for j in 0..SELECTED_PARAMETER_SET.k as usize {
+            a[i][j] = sampleNTT(rho, j as u8, i as u8);
         }
-
-        for i in 0..SELECTED_PARAMETER_SET.k as usize {
-            s[i] = samplePolyCBD(SELECTED_PARAMETER_SET.eta1, prf(SELECTED_PARAMETER_SET.eta1 as usize, sigma, n))
-        }
-
     }
+
+    for i in 0..SELECTED_PARAMETER_SET.k as usize {
+        s[i] = samplePolyCBD(SELECTED_PARAMETER_SET.eta1, prf(SELECTED_PARAMETER_SET.eta1 as usize, sigma, n))
+    }
+
 }
