af44aea4ceac78c984d93076580abba1613cd02c
Gvinfinity <gabrielsoares@berry.dev.br>
2026-02-01 00:22:09 -0300

    feat: Implement traits on polynomial

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Index, IndexMut};
+use std::ops::{Add, Index, IndexMut, Mul, Rem};
 use crate::SELECTED_PARAMETER_SET;
 
 #[derive(Clone, Debug)]
@@ -35,8 +35,66 @@ impl<T: num_traits::PrimInt, const N: usize> Add for Polynomial<T, N> {
     }
 }
 
+impl<T: num_traits::PrimInt, const N: usize> Mul<T> for Polynomial<T, N>{
+    type Output = Polynomial<T, N>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        let mut output: [T;N] = [T::zero();N];
+
+        for i in 0..N {
+            output[i] = self[i] * rhs;
+        }
+
+        Polynomial::new(&output)
+    }
+}
+
+impl<T: num_traits::PrimInt, const N: usize> Rem<T> for Polynomial<T, N>{
+    type Output = Polynomial<T, N>;
+    
+    fn rem(self, rhs: T) -> Self::Output {
+        let mut output: [T;N] = [T::zero();N];
+
+        for i in 0..N {
+            output[i] = self[i] % rhs;
+        }
+
+        Polynomial::new(&output)
+    }
+}
+
+
+impl<T: num_traits::PrimInt, const N: usize> IntoIterator for Polynomial<T, N> {
+    type Item = T;
+    type IntoIter = PolynomialIterator<T, N>;
+    
+    fn into_iter(self) -> Self::IntoIter {
+        PolynomialIterator {
+            polynomial: self,
+            index: 0
+        }
+    }
+}
+
+
 impl<T: num_traits::PrimInt, const N: usize> Polynomial<T, N> {
     pub fn new(coefficients: &[T;N]) -> Polynomial<T, N> {
         return Polynomial { 0: coefficients.clone() }
+    }
+}
+struct PolynomialIterator<T: num_traits::PrimInt, const N: usize> {
+    polynomial: Polynomial<T, N>,
+    index: usize
+}
+
+impl<T: num_traits::PrimInt, const N: usize> Iterator for PolynomialIterator<T, N> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index <= N {
+            return Some(self.polynomial[self.index]);
+        } else {
+            return None;
+        }
     }
 }
